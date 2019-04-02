09a8fd45a6010e69d2ed28700df93d678ea4c6f0
Eduardo Zluhan Samrsla <ed.zluhan@gmail.com>
2019-04-02 15:33:34 -0300

    view list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,30 @@
 package com.twu.biblioteca;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(greet());
+        System.out.println(getListOfBooks());
     }
 
     public static String greet() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
     }
+
+
+    public static List<String> getListOfBooks() {
+
+        List<String> listOfBooks = new ArrayList<String>();
+
+        listOfBooks.add("TDD By Example");
+        listOfBooks.add("The Clean Coder");
+
+        return listOfBooks;
+    }
+
+
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
@@ -11,6 +15,20 @@ public class BibliotecaAppTest {
         assertEquals(
                 "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!",
                 BibliotecaApp.greet()
+        );
+    }
+
+    @Test
+    public void sholdHaveAListOfBooks() {
+        BibliotecaApp biblioteca = new BibliotecaApp();
+
+        List<String> books = new ArrayList<String>();
+        books.add("TDD By Example");
+        books.add("The Clean Coder");
+
+        assertThat(
+                biblioteca.getListOfBooks(),
+                is(books)
         );
     }
 }
