56dd0782279e3e0b0336ec45a7db1d99cd1b4934
AlineFreitas <aline.do.freitas@gmail.com>
2019-04-05 10:43:49 -0300

    Deleted unused files

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 
 public class BibliotecaApp {
 
-    static Printer printer = new Printer(System.out);
-
     public static void main(String[] args) {
 
         System.out.println(greet());
